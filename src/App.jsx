dc771b3d3add8c0bcf1dc10100ccf5e531d6bc3d
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-12-02 07:24:48 +0300

    Upd patterns

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
-import Bulbs from "./Bulbs";
-import {PatternSelector, patterns} from "./PatternSelector";
-
+import React, { useState } from 'react'
+import Bulbs from './Bulbs'
+import { PatternSelector, patterns } from './PatternSelector'
 
 const App = () => {
+	const [selectedPattern, setSelectedPattern] = useState(
+		Array(4).fill(patterns[''])
+	)
 
-  const [selectedPattern, setSelectedPattern] = useState([patterns[''],patterns[''],patterns[''],patterns['']]);
+	const handlePatternSelect = (patternToHandle) => {
+		setSelectedPattern(patternToHandle)
+	}
 
-  const handlePatternSelect = (patternToHandle) => {
-    setSelectedPattern(patternToHandle);
-  };
+	console.log(selectedPattern)
+	return (
+		<>
+			<div className='screen-container'>
+				{selectedPattern.map((el) => (
+					<Bulbs pattern={el} />
+				))}
+			</div>
+			<PatternSelector onPatternSelect={handlePatternSelect} />
+		</>
+	)
+}
 
-  console.log(selectedPattern)
-  return (
-    <>
-      <div className='screen-container'>
-        {selectedPattern.map(el => <Bulbs pattern={el} />)}
-      </div>
-        <PatternSelector onPatternSelect={handlePatternSelect} />
-    </>
-  );
-};
-
-
-export default App;
+export default App
